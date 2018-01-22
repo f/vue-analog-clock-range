469850e3735e0ef204a85c05b1713dadc9e88650
Hatice Edis <hedis285@gmail.com>
2018-01-22 21:10:10 +0300

    add numeric clock (#2)
    * add numeric clock
    
    * add numeric clock
    
    * add screenshot numeric clock
    
    * delete canvas of id
    
    * add numeric clock

--- src/vue-analog-clock-range.vue
+++ src/vue-analog-clock-range.vue
@@ -35,7 +35,7 @@
       i++
     }
   }
-
+  
   function toTime(time) {
     return (new Date(`1970-01-01 ${time}`)).getTime();
   }
@@ -43,7 +43,83 @@
   function diffAsDegree(diff) {
     return Math.floor(diff / 60000) / 2;
   }
+  //  start draw clock //
+  function MakeField(ctx, radius, color, backgroundColor, borderColor , gradColor , boxColor) {
+    var grad;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2*Math.PI);
+    ctx.fillStyle = backgroundColor;
+    ctx.fill();
+    grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+    grad.addColorStop(0, gradColor);
+    grad.addColorStop(0.5, boxColor);
+    grad.addColorStop(1, borderColor);
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius*0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+  }
 
+  function MakeNumbers(ctx, radius) {
+    var ang;
+    var clock;
+    ctx.font = "bold "+radius*0.18 + "px arial";
+    ctx.textBaseline="middle";
+    ctx.textAlign="center";
+    for(clock = 1; clock < 13; clock++){
+      ang = clock * Math.PI / 6;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius*0.7);
+      ctx.rotate(-ang);
+      ctx.fillText(clock.toString(), 0, 0);
+      ctx.rotate(ang);
+      ctx.translate(0, radius*0.7);
+      ctx.rotate(-ang);
+    }
+  }
+
+  function MakeGrad(ctx, radius){
+      var now = new Date();
+      var hour = now.getHours();
+      var minute = now.getMinutes();
+      var second = now.getSeconds();
+      // draw hour
+      hour=hour%12;
+      hour=(hour*Math.PI/6)+
+      (minute*Math.PI/(6*60))+
+      (second*Math.PI/(360*60));
+      makeTime(ctx, hour, radius*0.5, radius*0.07);
+      // draw minute
+      minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+      makeTime(ctx, minute, radius*0.65, radius*0.07);
+      //  draw second
+      second=(second*Math.PI/30);
+      makeTime(ctx, second, radius*0.9, radius*0.02);
+  }
+
+  function makeTime(ctx, pos, length, width) {
+      ctx.beginPath();
+      ctx.lineWidth = width;
+      ctx.lineCap = "round";
+      ctx.moveTo(0,0);
+      ctx.rotate(pos);
+      ctx.lineTo(0, -length);
+      ctx.stroke();
+      ctx.rotate(-pos);
+  }
+
+function MakeClock({ ctx, radius, backgroundColor, rangeColor, borderColor, gradColor, boxColor }){
+  let r = radius / 2;
+  ctx.translate(r,r);
+  r = r * 0.90;
+  MakeField(ctx, r, rangeColor, backgroundColor, borderColor, gradColor, boxColor);
+  MakeNumbers(ctx, r);
+  MakeGrad(ctx, r);
+}
+// finish draw clock  //
   function draw({ ctx, radius, start, end, backgroundColor, rangeColor,
     showDecor, decorColor }) {
     const r = radius / 2;
@@ -62,6 +138,10 @@
   export default {
     props: {
       showDecor: {
+        type: Boolean,
+        default: false,
+      },
+      isNumeric: {
         type: Boolean,
         default: false,
       },
@@ -87,6 +167,18 @@
         type: Number,
         default: 200,
       },
+      borderColor : {
+        type: String,
+        default: 'transparent',
+      },
+      gradColor : {
+        type: String,
+        default : '#cccccc',
+      },
+      boxColor: {
+        type: String,
+        default: 'transparent',
+      }
     },
     computed: {
       ctx() {
@@ -94,11 +186,21 @@
       },
     },
     mounted() {
-      draw(this);
+      if (this.isNumeric) {
+        MakeClock(this);
+        let specialRadius = this.radius/2*0.9
+        setInterval(()=> {
+          MakeField(this.ctx, specialRadius , this.rangeColor, this.backgroundColor, this.borderColor , this.gradColor ,this.boxColor);
+          MakeGrad(this.ctx, specialRadius);
+          MakeNumbers(this.ctx, specialRadius);
+        },1000)
+      } else{
+        draw(this);
+      }
     },
     watch: {
-      start() { draw(this); },
-      end() { draw(this); },
+      start() { if(!this.isNumeric) { draw(this); }},
+      end() { if(!this.isNumeric) {draw(this);} },
     },
   };
 </script>
